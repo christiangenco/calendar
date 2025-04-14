794a5b17e72d6d2008a5990bcd88cfe096d6aa03
Christian Genco <christian@gen.co>
2025-04-14 10:24:54 -0500

    deployments

---
+++ x/deploy.sh
@@ -0,0 +1,11 @@
+# Build the project
+yarn build
+
+NAME="calendar"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../out" && pwd )"
+
+aws s3 sync $DIR s3://gen.co/$NAME/ --acl=public-read \
+  --exclude ".DS_Store" \
+  --exclude ".git/*"
+
+aws cloudfront create-invalidation --distribution-id EXW76U2SHH4YQ --paths /$NAME/ /$NAME/index.html /$NAME/favicon.ico /$NAME/script.js
